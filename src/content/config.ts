2c26b8a2c0504a15b92634ce10ea71931d81a7b5
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-03-21 21:38:31 +0100

    refactor(content) change projects data type

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,13 @@
 import { defineCollection, z } from 'astro:content';
 
 const projectsCollection = defineCollection({
-	schema: z.object({
-		inProgress: z.boolean(),
+	schema: ({image}) => z.object({
 		title: z.string(),
 		description: z.string(),
+		image: image(),
+		alt: z.string(),
+		url: z.string().default('#'),
 		tags: z.array(z.string()),
-		link: z.string(),
-		img_alt: z.string().optional(),
 	}),
 });
 
