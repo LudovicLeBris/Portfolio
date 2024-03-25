217b7fabe11c6f71e8ef8cfbdd7aae174988bc40
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-03-25 17:12:05 +0100

    feat(config) adding color to Tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		extend: {
 			colors: {
 				'primary-light': '#509046',
-				'primary-dark': '#1C5812'
+				'primary-dark': '#1C5812',
+				'bg-dark': '#0B2107',
 			}
 		},
 	},
